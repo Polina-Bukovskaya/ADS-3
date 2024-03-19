aacb873b1e9ba5e667e60a8a3641ce53e8a0d5d8
Polina-Bukovskaya <114609935+Polina-Bukovskaya@users.noreply.github.com>
2024-03-19 17:07:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,29 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+  int leftBound = 0;
+  int rightBound = size - 1;
+  int count = 0;
+  while (leftBound <= rightBound) {
+    int center = (leftBound + rightBoundr) / 2;
+    if (arr[center] == value) {
+      count++;
+      int buf = center - 1;
+      while (buf >= leftBound && arr[buf] == value) {
+        count++;
+        buf--;
+      }
+      buf = center + 1;
+      while (buf <= rightBound && arr[buf] == value) {
+        count++;
+        buf++;
+      }
+      return count;
+    } else if (arr[center] < value) {
+      leftBound = center + 1;
+    } else {
+      rightBound = center - 1;
+    }
+  }
   return 0; // если ничего не найдено
 }
